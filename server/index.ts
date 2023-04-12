48a7291aa046bd5addd4f2b0157f8b355d765e04
LuxianAiolos <luxianaiolos@gmail.com>
2023-04-12 14:06:43 -0700

    Created Mongoose/mongo connection

--- server/index.ts
+++ server/index.ts
@@ -1,37 +1,51 @@
-import { writeFile } from 'fs'
-// import { generate } from './types/shortid'
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import colors from 'colors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 
-
-
-//// use interfaces for objects
-interface Params {
-  a: number
-  b: number
-}
-
-///// use types for everyting else
-type Params2 = {
-  a: number
-  b: number
-}
-const add = (a: number, b?: number) => {
-  if (b) {
-    return a+b
-  } else {
-    return a
-  }
-
-  return a + b!  // can add ! to make ts stop complaining ( escape hatch )
-}
-
-
+require('dotenv').config()
+mongoose.set("strictQuery", false)
 const PORT = 8080
-
+const URI = process.env.ATLAS_URI
+const options = { useNewUrlParser: true, useUnifiedTypology: true }
 const app = express()
 app.use(cors)
+app.use('/', require('./routes/todoRoutes'))
+
+const connectDB = async (URI: string, options: object) => {
+  try {
+    const connection = await mongoose.connect(URI, options)
+    console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline)
+  } catch (err) {
+    throw err
+  }
+}
+
 
 
 app.listen(PORT, ()=> console.log('listening on', PORT))
+
+
+// //// use interfaces for objects
+// interface Params {
+//   a: number
+//   b: number
+// }
+
+// ///// use types for everyting else
+// type Params2 = {
+//   a: number
+//   b: number
+// }
+// const add = (a: number, b?: number) => {
+//   if (b) {
+//     return a+b
+//   } else {
+//     return a
+//   }
+
+//   return a + b!  // can add ! to make ts stop complaining ( escape hatch )
+// }
